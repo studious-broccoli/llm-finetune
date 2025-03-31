c6c38b955e557af2ff85bd43296160a7f606620d
studious-broccoli <ariannampryor@gmail.com>
2025-03-31 14:32:37 -0600

    removed dead code

--- llm-finetune.py
+++ llm-finetune.py
@@ -146,17 +146,6 @@ model = get_peft_model(model, lora_config)
 model.print_trainable_parameters()
 
 
-# # --------------------------------------------
-# # Evaluate model
-# # --------------------------------------------
-# metric = evaluate.load("accuracy")
-#
-# # def compute_metrics(eval_pred):
-# #    logits, labels = eval_pred
-# #    predictions = np.argmax(logits, axis=-1)
-# #    return metric.compute(predictions=predictions, references=labels)
-
-
 # --------------------------------------------
 # Training arguments
 # --------------------------------------------
@@ -191,21 +180,11 @@ mlflow.log_params({
 # --------------------------------------------
 # Trainer
 # --------------------------------------------
-# trainer = Trainer(
-#     model=model,
-#     args=training_args,
-#     train_dataset=tokenized_dataset["train"],
-#     tokenizer=tokenizer,
-#     data_collator=data_collator,
-# )
-
 trainer = Trainer(
    model=model,
    args=training_args,
    train_dataset=small_train_dataset,
    eval_dataset=small_eval_dataset,
-   # compute_metrics=compute_metrics,
-
 )
 
 
